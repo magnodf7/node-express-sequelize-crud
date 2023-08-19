e25cef5ab1ab94d09f24c37e803c77925095451b
magnodf7 <magno.brunodf7@gmail.com>
2023-08-19 11:21:45 -0300

    adding settings files

---
+++ app/models/index.js
@@ -0,0 +1,22 @@
+const dbconfig = require ("../config/db.config.js");
+
+const Sequelize = require ("sequelize");
+const dbConfig = require("../config/db.config.js");
+const sequelize = new Sequelize(dbconfig.DB, dbconfig. USER, dbconfig.PASSWORD, {
+    host: dbconfig.HOST,
+    dialect: dbconfig.dialect,
+    operatorsALIASES: false,
+
+    pool: {
+        max: dbconfig.pool.max,
+        min: dbconfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+    }
+});
+
+const db ={};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
